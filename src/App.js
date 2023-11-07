42bacf507f95d25c44eb512986d55bbc65652cc3
Yutae <tm1361@naver.com>
2023-11-07 17:41:13 +0900

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React,{useState} from 'react'
+
+import TestPage from "./route/TestPage";
+import ResultPage from "./route/ResultPage";
+import MainPage from "./route/MainPage";
 
 function App() {
+  const [EI, setEI] = useState(0);
+  const [SN, setSN] = useState(0);
+  const [TF, setTF] = useState(0);
+  const [JP, setJP] = useState(0);
+ 
+  const [datas, setDatas] = useState();
+  const [MBTI, setMBTI] = useState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage setEI={setEI} setSN={setSN} setTF={setTF} setJP={setJP}/>}></Route>
+        <Route path="/testpage" element={<TestPage 
+        EI={EI}
+        SN={SN}
+        TF={TF}
+        JP={JP}
+        setEI={setEI} setSN={setSN} setTF={setTF} setJP={setJP} MBTI={MBTI} setMBTI={setMBTI}datas={datas} setDatas={setDatas}/>}></Route>
+        <Route path="/resultpage" element={<ResultPage MBTI={MBTI} datas={datas}/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
